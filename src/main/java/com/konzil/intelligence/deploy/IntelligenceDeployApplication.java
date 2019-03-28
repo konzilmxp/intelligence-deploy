d4bb2fae536286b91a52fe785e2ed64be230485d
maxiaoping <15372426005@189.cn>
2019-03-28 20:47:53 +0800

    idea整合GitHub

---
+++ src/main/java/com/konzil/intelligence/deploy/IntelligenceDeployApplication.java
@@ -0,0 +1,13 @@
+package com.konzil.intelligence.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligenceDeployApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/konzil/intelligence/deploy/config/web/IntelligenceDeployViewConfig.java
@@ -0,0 +1,23 @@
+package com.konzil.intelligence.deploy.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+* @ClassName: IntelligenceDeployViewConfig
+* @Author: KONZIL
+* @description: web配置
+* @date: 2019-03-28
+*/
+@Configuration
+public class IntelligenceDeployViewConfig implements WebMvcConfigurer {
+    /**
+     * 静态资源映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    }
+}
